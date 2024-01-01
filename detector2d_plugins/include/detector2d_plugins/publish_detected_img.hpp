deef5479f4aa4c628d114a3e54f56b51b11594a7
pusin819 <shibata0819haru@gmail.com>
2024-01-02 00:58:29 +0900

    add yolox_ros and include

---
+++ detector2d_plugins/include/detector2d_plugins/publish_detected_img.hpp
@@ -0,0 +1,50 @@
+#pragma once
+
+#include <cmath>
+#include <chrono>
+
+#include <image_transport/image_transport.hpp>
+#include <cv_bridge/cv_bridge.h>
+#include <rclcpp/rclcpp.hpp>
+#include <rclcpp_components/register_node_macro.hpp>
+
+#include "bboxes_ex_msgs/msg/bounding_box.hpp"
+#include "bboxes_ex_msgs/msg/bounding_boxes.hpp"
+
+#include <pluginlib/class_loader.hpp>
+
+#include "yolox_cpp/yolox.hpp"
+#include "yolox_cpp/utils.hpp"
+#include "yolox_param/yolox_param.hpp"
+
+#include <detector2d_base/detector2d_base.hpp>
+#include <detector2d_param/detector2d_param.hpp>
+#include <vision_msgs/msg/detection2_d_array.hpp>
+
+namespace detector2d_plugins{
+
+    class publish_detected_img : public detector2d_base::Detector
+    {
+    public:
+        void init(const detector2d_parameters::ParamListener &) override;
+        vision_msgs::msg::Detection2DArray detect(const cv::Mat &) override;
+    protected:
+        std::shared_ptr<yolox_parameters::ParamListener> param_listener_;
+        yolox_parameters::Params params_;
+    private:
+        rclcpp::TimerBase::SharedPtr init_timer_;
+
+        std::unique_ptr<yolox_cpp::AbcYoloX> yolox_;
+        std::vector<std::string> class_names_;
+
+        image_transport::Subscriber sub_image_;
+        void colorImageCallback(const sensor_msgs::msg::Image::ConstSharedPtr&);
+
+        rclcpp::Publisher<bboxes_ex_msgs::msg::BoundingBoxes>::SharedPtr pub_bboxes_;
+        image_transport::Publisher pub_image_;
+
+        bboxes_ex_msgs::msg::BoundingBoxes objects_to_bboxes(cv::Mat, std::vector<yolox_cpp::Object>, std_msgs::msg::Header);
+    };
+
+}//namespace detector2d_plugins
+
